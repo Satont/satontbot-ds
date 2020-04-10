d29b77f345a5fbff399d619dcfdb19039cfbb295
Satont <satontworldwide@gmail.com>
2020-04-10 05:01:09 +0300

    Attach dbUser to message member context

--- src/events/message.ts
+++ src/events/message.ts
@@ -2,6 +2,7 @@ import { Event } from '../typings/discordjs'
 import MarmokBot from '../client/marmokbot'
 import { Message } from 'discord.js'
 import CommandHandler from '../struct/CommandHandler'
+import { User } from '../models/User'
 
 export default class Ready implements Event {
   name = 'message'
@@ -14,6 +15,16 @@ export default class Ready implements Event {
   }
 
   async run(message: Message) {
+    await this.findOrUpdateUser(message)
     await this.commandHandler.process(message)
+  }
+
+  async findOrUpdateUser(message: Message) {
+    const [user] = await User.findOrCreate({
+      where: { guildId: message.guild.id, userId: message.member.id },
+      defaults: { guildId: message.guild.id, userId: message.member.id, username: message.author.username }
+    })
+
+    message.member.db = user
   }
 }
